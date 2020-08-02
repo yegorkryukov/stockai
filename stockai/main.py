d3bfbb6590990e08845b09d2d44d97b2adcf504d
Yegor Kryukov <yegor.kryukov@gmail.com>
2020-08-01 23:38:30 -0400

    Added appnope

--- stockai/main.py
+++ stockai/main.py
@@ -2,6 +2,7 @@ from config.config import create_logger
 from data_collection.extract_text import scrape_urls
 from yahoo_fin import stock_info as si 
 from multiprocessing import Pool
+import appnope
 
 if __name__ == '__main__':
     logger = create_logger('stockai.log')
@@ -11,10 +12,12 @@ if __name__ == '__main__':
     tickers = si.tickers_sp500()
     # tickers = ['MSFT'] #for testing
 
+    appnope.nope()
     p = Pool()
     result = p.map_async(scrape_urls, tickers)
 
     p.close()
     p.join()
+    appnope.nap()
 
     logger.info(f'Finished text extraction')
